b0eb4e8893a16a57e9b74b37094bda5969e45951
Saikiran Nagulapuram <20274@arthahyd.com>
2024-01-23 09:26:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// function sayHello(name){
+//     console.log("Good morning" + " " + name)
+// }
+// sayHello("Hari");
+
+// console.log(module);
+
+// console.log(__filename)
+// const log = require('./log');
+
+// log('asa');
+
+
+// const path = require('path');
+
+// var pathObj =path.parse(__filename, __dirname);
+
+// console.log(pathObj);
+
+
+
+// const os = require('os')
+
+// var tataoMem = os.totalmem();
+
+// var freeMen = os.freemem();
+
+// console.log(tataoMem);
+// console.log(freeMen);
+
+
+
+// const fs = require('fs');
+// // var file = fs.readdirSync('./');
+// // console.log(file);
+// fs.readdir('./',function(err, file){
+//     if(err)
+//      console.log(err);
+//     else(file)
+//      console.log(file);
+// })
+
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter;
+
+emitter.on('messagelog', (arg) =>{
+    console.log('listner called', arg)
+});
+emitter.emit('messagelog', {id:1, url:'http://'});
+
+
